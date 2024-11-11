45c56647ee5682105c6cf5e2edd88978f6e1f5e2
Dino <theminecraftdinoman@gmail.com>
2024-11-11 22:14:02 +0100

    lightsbaeber

--- src/main/java/net/awt/awt/item/ModItems.java
+++ src/main/java/net/awt/awt/item/ModItems.java
@@ -12,6 +12,7 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
 	public static final Item ARTRIUM = registerItem("artrium", new Item(new FabricItemSettings()));
+	public static final Item LIGHTSABER = registerItem("lightsaber", new Item(new FabricItemSettings()));
 
 	private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries) {
 		entries.add(ARTRIUM);
