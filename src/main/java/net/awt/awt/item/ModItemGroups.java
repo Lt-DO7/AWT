674363d773a9dd66fb13b7632f69f61393e29059
Lt-DO7 <alfieharrylansdell@gmail.com>
2024-11-28 15:30:18 +0000

    roundel

--- src/main/java/net/awt/awt/item/ModItemGroups.java
+++ src/main/java/net/awt/awt/item/ModItemGroups.java
@@ -95,6 +95,7 @@ public class ModItemGroups {
 						entries.add(ModBlocks.ORANGE_CONCRETE_ROUNDEL);
 						entries.add(ModBlocks.OXIDIZED_COPPER_ROUNDEL);
 						entries.add(ModBlocks.PINK_CONCRETE_ROUNDEL);
+						entries.add(ModBlocks.PURPLE_CONCRETE_ROUNDEL);
 					}).build());
 
 
