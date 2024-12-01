5b08c54b08cddd389d2badbcd37c6ed97a768507
Torchwood-1 <jacobroberts@virginmedia.com>
2024-12-01 00:43:06 +0000

    lime onion

---
+++ src/client/java/net/awt/awt/exterior/onion/ClientLimeOnionVariant.java
@@ -0,0 +1,14 @@
+package net.awt.awt.exterior.onion;
+
+import net.minecraft.util.Identifier;
+
+public class ClientLimeOnionVariant extends ClientOnionVariant {
+    public ClientLimeOnionVariant() {
+        super("lime");
+    }
+
+    @Override
+    public Identifier emission() {
+        return null;
+    }
+}
---
+++ src/main/java/net/awt/awt/exterior/onion/LimeOnionExteriorVariant.java
@@ -0,0 +1,7 @@
+package net.awt.awt.exterior.onion;
+
+public class LimeOnionExteriorVariant extends OnionExteriorVariant {
+    public LimeOnionExteriorVariant() {
+        super("lime");
+    }
+}
