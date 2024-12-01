4fe786ba66faf5137e46be4887af1c4bbf575220
Torchwood-1 <jacobroberts@virginmedia.com>
2024-12-01 00:43:52 +0000

    white onion

---
+++ src/client/java/net/awt/awt/exterior/onion/ClientWhiteOnionVariant.java
@@ -0,0 +1,14 @@
+package net.awt.awt.exterior.onion;
+
+import net.minecraft.util.Identifier;
+
+public class ClientWhiteOnionVariant extends ClientOnionVariant {
+    public ClientWhiteOnionVariant() {
+        super("white");
+    }
+
+    @Override
+    public Identifier emission() {
+        return null;
+    }
+}
---
+++ src/main/java/net/awt/awt/exterior/onion/WhiteOnionExteriorVariant.java
@@ -0,0 +1,7 @@
+package net.awt.awt.exterior.onion;
+
+public class WhiteOnionExteriorVariant extends OnionExteriorVariant {
+    public WhiteOnionExteriorVariant() {
+        super("white");
+    }
+}
