316b16b9be14841100f36eedc7e58843b6e52bf9
Torchwood-1 <jacobroberts@virginmedia.com>
2024-12-01 00:43:32 +0000

    pink onion

---
+++ src/client/java/net/awt/awt/exterior/onion/ClientPinkOnionVariant.java
@@ -0,0 +1,14 @@
+package net.awt.awt.exterior.onion;
+
+import net.minecraft.util.Identifier;
+
+public class ClientPinkOnionVariant extends ClientOnionVariant {
+    public ClientPinkOnionVariant() {
+        super("pink");
+    }
+
+    @Override
+    public Identifier emission() {
+        return null;
+    }
+}
---
+++ src/main/java/net/awt/awt/exterior/onion/PinkOnionExteriorVariant.java
@@ -0,0 +1,7 @@
+package net.awt.awt.exterior.onion;
+
+public class PinkOnionExteriorVariant extends OnionExteriorVariant {
+    public PinkOnionExteriorVariant() {
+        super("pink");
+    }
+}
