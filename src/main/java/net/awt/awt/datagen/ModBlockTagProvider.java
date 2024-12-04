a757bd8360315f0cd313a988eb657cb4f273ae26
Torchwood-1 <jacobroberts@virginmedia.com>
2024-12-04 18:03:48 +0000

    u dud sine stuff here

--- src/main/java/net/awt/awt/datagen/ModBlockTagProvider.java
+++ src/main/java/net/awt/awt/datagen/ModBlockTagProvider.java
@@ -16,6 +16,8 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
         super(output, registriesFuture);
     }
 
+
+
     @Override
     protected void configure(RegistryWrapper.WrapperLookup arg) {
         getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
@@ -29,8 +31,34 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                 .add(ModBlocks.DALEKANIUM_BLOCK)
                 .add(ModBlocks.DEEPSLATE_ARTRIUM_ORE);
 
-
-        getOrCreateTagBuilder(BlockTags.NEEDS_STONE_TOOL);
+        getOrCreateTagBuilder(BlockTags.NEEDS_STONE_TOOL)
+                .add(ModBlocks.WHITE_CONCRETE_ROUNDEL)
+                .add(ModBlocks.BLACK_CONCRETE_ROUNDEL)
+                .add(ModBlocks.END_STONE_BRICKS_ROUNDEL)
+                .add(ModBlocks.GRAY_CONCRETE_ROUNDEL)
+                .add(ModBlocks.GREEN_CONCRETE_ROUNDEL)
+                .add(ModBlocks.LIGHT_GRAY_CONCRETE_ROUNDEL)
+                .add(ModBlocks.POLISHED_ANDESITE_ROUNDEL)
+                .add(ModBlocks.POLISHED_DEEPSLATE_ROUNDEL)
+                .add(ModBlocks.POLISHED_GRANITE_ROUNDEL)
+                .add(ModBlocks.POLISHED_DIORITE_ROUNDEL)
+                .add(ModBlocks.QUARTZ_ROUNDEL)
+                .add(ModBlocks.SANDSTONE_ROUNDEL)
+                .add(ModBlocks.CYAN_CONCRETE_ROUNDEL)
+                .add(ModBlocks.YELLOW_CONCRETE_ROUNDEL)
+                .add(ModBlocks.WEATHERED_COPPER_ROUNDEL)
+                .add(ModBlocks.RED_CONCRETE_ROUNDEL)
+                .add(ModBlocks.PURPLE_CONCRETE_ROUNDEL)
+                .add(ModBlocks.PINK_CONCRETE_ROUNDEL)
+                .add(ModBlocks.BROWN_CONCRETE_ROUNDEL)
+                .add(ModBlocks.ORANGE_CONCRETE_ROUNDEL)
+                .add(ModBlocks.OXIDIZED_COPPER_ROUNDEL)
+                .add(ModBlocks.LIME_CONCRETE_ROUNDEL)
+                .add(ModBlocks.LIGHT_BLUE_CONCRETE_ROUNDEL)
+                .add(ModBlocks.BLUE_CONCRETE_ROUNDEL)
+                .add(ModBlocks.EXPOSED_COPPER_ROUNDEL)
+                .add(ModBlocks.COPPER_ROUNDEL)
+        ;
 
         getOrCreateTagBuilder(BlockTags.NEEDS_IRON_TOOL)
                 .add(ModBlocks.ARTRIUM_BLOCK)
@@ -49,5 +77,20 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
         getOrCreateTagBuilder(BlockTags.WALLS);
 
         getOrCreateTagBuilder(BlockTags.LOGS_THAT_BURN);
+
+        getOrCreateTagBuilder(BlockTags.AXE_MINEABLE)
+                .add(ModBlocks.STRIPPED_ACACIA_LOG_ROUNDEL)
+                .add(ModBlocks.STRIPPED_BIRCH_LOG_ROUNDEL)
+                .add(ModBlocks.STRIPPED_CHERRY_LOG_ROUNDEL)
+                .add(ModBlocks.STRIPPED_DARK_OAK_LOG_ROUNDEL)
+                .add(ModBlocks.STRIPPED_JUNGLE_LOG_ROUNDEL)
+                .add(ModBlocks.STRIPPED_MANGROVE_LOG_ROUNDEL)
+                .add(ModBlocks.STRIPPED_OAK_LOG_ROUNDEL)
+                .add(ModBlocks.STRIPPED_SPRUCE_LOG_ROUNDEL);
+
+        getOrCreateTagBuilder(BlockTags.SHOVEL_MINEABLE)
+                .add(ModBlocks.MOSS_ROUNDEL)
+                .add(ModBlocks.DIRT_ROUNDEL);
+
     }
 }
--- src/main/java/net/awt/awt/datagen/ModLootTableProvider.java
+++ src/main/java/net/awt/awt/datagen/ModLootTableProvider.java
@@ -28,7 +28,44 @@ public class ModLootTableProvider extends FabricBlockLootTableProvider {
         addDrop(ModBlocks.ARTRIUM_ORE, copperLikeOreDrops(ModBlocks.ARTRIUM_ORE, ModItems.ARTRIUM));
         addDrop(ModBlocks.DEEPSLATE_ARTRIUM_ORE, copperLikeOreDrops(ModBlocks.DEEPSLATE_ARTRIUM_ORE, ModItems.ARTRIUM));
         addDrop(ModBlocks.ARTRIUM_BLOCK, drops(ModBlocks.ARTRIUM_BLOCK));
+        addDrop(ModBlocks.MOSS_ROUNDEL, drops(ModBlocks.MOSS_ROUNDEL));
+        addDrop(ModBlocks.DIRT_ROUNDEL, drops(ModBlocks.DIRT_ROUNDEL));
+        addDrop(ModBlocks.END_STONE_BRICKS_ROUNDEL, drops(ModBlocks.END_STONE_BRICKS_ROUNDEL));
+        addDrop(ModBlocks.POLISHED_ANDESITE_ROUNDEL, drops(ModBlocks.POLISHED_ANDESITE_ROUNDEL));
+        addDrop(ModBlocks.POLISHED_DEEPSLATE_ROUNDEL, drops(ModBlocks.POLISHED_DEEPSLATE_ROUNDEL));
+        addDrop(ModBlocks.POLISHED_DIORITE_ROUNDEL, drops(ModBlocks.POLISHED_DIORITE_ROUNDEL));
+        addDrop(ModBlocks.POLISHED_GRANITE_ROUNDEL, drops(ModBlocks.POLISHED_GRANITE_ROUNDEL));
+        addDrop(ModBlocks.QUARTZ_ROUNDEL, drops(ModBlocks.QUARTZ_ROUNDEL));
+        addDrop(ModBlocks.SANDSTONE_ROUNDEL, drops(ModBlocks.SANDSTONE_ROUNDEL));
+        addDrop(ModBlocks.STRIPPED_ACACIA_LOG_ROUNDEL, drops(ModBlocks.STRIPPED_ACACIA_LOG_ROUNDEL));
+        addDrop(ModBlocks.STRIPPED_BIRCH_LOG_ROUNDEL, drops(ModBlocks.STRIPPED_BIRCH_LOG_ROUNDEL));
+        addDrop(ModBlocks.STRIPPED_CHERRY_LOG_ROUNDEL, drops(ModBlocks.STRIPPED_CHERRY_LOG_ROUNDEL));
+        addDrop(ModBlocks.STRIPPED_DARK_OAK_LOG_ROUNDEL, drops(ModBlocks.STRIPPED_DARK_OAK_LOG_ROUNDEL));
+        addDrop(ModBlocks.STRIPPED_JUNGLE_LOG_ROUNDEL, drops(ModBlocks.STRIPPED_JUNGLE_LOG_ROUNDEL));
+        addDrop(ModBlocks.STRIPPED_MANGROVE_LOG_ROUNDEL, drops(ModBlocks.STRIPPED_MANGROVE_LOG_ROUNDEL));
+        addDrop(ModBlocks.STRIPPED_OAK_LOG_ROUNDEL, drops(ModBlocks.STRIPPED_OAK_LOG_ROUNDEL));
+        addDrop(ModBlocks.STRIPPED_SPRUCE_LOG_ROUNDEL, drops(ModBlocks.STRIPPED_SPRUCE_LOG_ROUNDEL));
+        addDrop(ModBlocks.COPPER_ROUNDEL, drops(ModBlocks.COPPER_ROUNDEL));
+        addDrop(ModBlocks.EXPOSED_COPPER_ROUNDEL, drops(ModBlocks.EXPOSED_COPPER_ROUNDEL));
+        addDrop(ModBlocks.OXIDIZED_COPPER_ROUNDEL, drops(ModBlocks.OXIDIZED_COPPER_ROUNDEL));
+        addDrop(ModBlocks.WEATHERED_COPPER_ROUNDEL, drops(ModBlocks.WEATHERED_COPPER_ROUNDEL));
+        addDrop(ModBlocks.ORANGE_CONCRETE_ROUNDEL, drops(ModBlocks.ORANGE_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.PINK_CONCRETE_ROUNDEL, drops(ModBlocks.PINK_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.PURPLE_CONCRETE_ROUNDEL, drops(ModBlocks.PURPLE_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.RED_CONCRETE_ROUNDEL, drops(ModBlocks.RED_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.YELLOW_CONCRETE_ROUNDEL, drops(ModBlocks.YELLOW_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.WHITE_CONCRETE_ROUNDEL, drops(ModBlocks.WHITE_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.BLACK_CONCRETE_ROUNDEL, drops(ModBlocks.BLACK_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.CYAN_CONCRETE_ROUNDEL, drops(ModBlocks.CYAN_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.LIGHT_BLUE_CONCRETE_ROUNDEL, drops(ModBlocks.LIGHT_BLUE_CONCRETE_ROUNDEL));
 
+        addDrop(ModBlocks.LIME_CONCRETE_ROUNDEL, drops(ModBlocks.LIME_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.MAGENTA_CONCRETE_ROUNDEL, drops(ModBlocks.MAGENTA_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.BLUE_CONCRETE_ROUNDEL, drops(ModBlocks.BLUE_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.BROWN_CONCRETE_ROUNDEL, drops(ModBlocks.BROWN_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.GRAY_CONCRETE_ROUNDEL, drops(ModBlocks.GRAY_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.GREEN_CONCRETE_ROUNDEL, drops(ModBlocks.GREEN_CONCRETE_ROUNDEL));
+        addDrop(ModBlocks.LIGHT_GRAY_CONCRETE_ROUNDEL, drops(ModBlocks.LIGHT_GRAY_CONCRETE_ROUNDEL));
 
     }
 
