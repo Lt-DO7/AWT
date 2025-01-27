61ab807fdb52b12434353d1d8f0960ab667b2920
Dino <theminecraftdinoman@gmail.com>
2025-01-27 04:38:13 +0100

    dice i set everything up if you want to fuck around with shit

--- src/main/java/net/awt/awt/item/custom/VortexManipulator.java
+++ src/main/java/net/awt/awt/item/custom/VortexManipulator.java
@@ -1,10 +1,28 @@
 package net.awt.awt.item.custom;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Rarity;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+
+import javax.naming.Context;
 
 public class VortexManipulator extends Item {
     public VortexManipulator(Settings settings) {
         super(settings.rarity(Rarity.RARE).maxCount(1).maxDamage(100).fireproof());
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if (!world.isClient) {
+
+        }
+        return super.use(world, user, hand);
     }
 }
