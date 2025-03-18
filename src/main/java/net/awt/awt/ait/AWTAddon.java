a20248dc7277ac28d20ee4f96c005f163e70f9c9
Lt-DO7 <alfieharrylansdell@gmail.com>
2025-03-18 22:09:32 +0000

    idfk anymore

---
+++ src/main/java/net/awt/awt/ait/AWTAddon.java
@@ -0,0 +1,11 @@
+package net.awt.awt.ait;
+
+import dev.amble.ait.api.AITModInitializer;
+
+public class AWTAddon implements AITModInitializer {
+
+    @Override
+    public void onInitializeAIT() {
+
+    }
+}
---
+++ src/main/java/net/awt/awt/amblekit/AmbleInitializer.java
@@ -0,0 +1,10 @@
+package net.awt.awt.amblekit;
+
+import dev.amble.lib.api.AmbleKitInitializer;
+
+public class AmbleInitializer implements AmbleKitInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
