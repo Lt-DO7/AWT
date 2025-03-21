f3fb708573b48801111c0741ddac590ad825c698
Lt-DO7 <alfieharrylansdell@gmail.com>
2025-03-21 19:36:47 +0000

    Fixed the AITUtil file

---
+++ src/main/java/util/AITUtil.java
@@ -0,0 +1,26 @@
+package net.awt.awt.util;
+
+import loqor.ait.api.AITModInitializer;
+import net.awt.awt.AWT;
+
+public class AITUtil implements AITModInitializer {
+    private static AITUtil instance;
+
+    @Override
+    public void onInitializeAIT() {
+        // this is called when AIT is ready to be registered to and used with, ait related logic should be placed here
+        if (isAITLoaded()) return;
+
+        AWT.LOGGER.info("Initializing AITUtil");
+        instance = this;
+
+    }
+
+    public static AITUtil instance() {
+        return instance;
+    }
+
+    public static boolean isAITLoaded() {
+        return instance() != null;
+    }
+}
