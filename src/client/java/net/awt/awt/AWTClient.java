2f5a41b282aae4313bdd9c966896f3075ac44cd2
Lt-DO7 <alfieharrylansdell@gmail.com>
2025-03-23 00:15:43 +0000

    deleted more shit i needed for custom exteriors because i cant its all cooked man

--- src/client/java/net/awt/awt/AWTClient.java
+++ src/client/java/net/awt/awt/AWTClient.java
@@ -1,9 +1,6 @@
 package net.awt.awt;
 
-import net.awt.client.models.exteriors.BakerExteriorModel;
-import net.awt.client.renderers.monitors.AWTMonitorRenderer;
-import net.awt.client.renderers.monitors.AWTScreenMonitorRenderer;
-import net.awt.core.AWTBlockEntityTypes;
+
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
@@ -11,8 +8,7 @@ public class AWTClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
-        registerClientAddonExteriors();
-        blockEntityRendererRegister();
+
     }
 
 }
