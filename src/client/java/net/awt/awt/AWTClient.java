b5d9bb0aa60040f05f4f6ee06f3e6cb810319d16
Lt-DO7 <alfieharrylansdell@gmail.com>
2025-03-23 00:14:33 +0000

    Lucifer you are gonna have to add custom exteriors n shit i just cant my blockbench is fucked and that means i cant get what i need

--- src/client/java/net/awt/awt/AWTClient.java
+++ src/client/java/net/awt/awt/AWTClient.java
@@ -1,11 +1,9 @@
 package net.awt.awt;
 
-import net.awt.client.models.doors.BakerDoorModel;
 import net.awt.client.models.exteriors.BakerExteriorModel;
 import net.awt.client.renderers.monitors.AWTMonitorRenderer;
 import net.awt.client.renderers.monitors.AWTScreenMonitorRenderer;
 import net.awt.core.AWTBlockEntityTypes;
-import dev.amble.ait.client.models.doors.CapsuleDoorModel;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
@@ -16,17 +14,5 @@ public class AWTClient implements ClientModInitializer {
         registerClientAddonExteriors();
         blockEntityRendererRegister();
     }
-
-    private void registerClientAddonExteriors() {
-        AWT.Baker.setModel(new BakerExteriorModel()).toClient().register();
-        AWT.Baker.toDoor().setModel(new BakerDoorModel(BakerDoorModel.getTexturedModelData().createModel())).toClient().register();
-
-    };
-    public static void blockEntityRendererRegister() {
-
-        BlockEntityRendererFactories.register(AWTTypes.AWT_MONITOR_BLOCK_ENTITY_TYPE, AWTMonitorRenderer::new);
-        BlockEntityRendererFactories.register(AWTBlockEntityTypes.AWT_SCREEN_MONITOR_BLOCK_ENTITY_TYPE, AWTScreenMonitorRenderer::new);
-    }
-}
 
 }
